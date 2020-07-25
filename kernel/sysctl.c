8bf587126150339f245356cfcf303e6abc68371d
Kailash <kailash.sudhakar@gmail.com>
2020-07-26 04:57:55 +0530

    Revert "sched: Use initial_task_util load for new tasks"
    This reverts commit 57eb071cd3b24d0171234ac56de25e2ae4ab4bf2.
    
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -356,13 +356,6 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one,
 	},
 	{
-		.procname	= "sched_initial_task_util",
-		.data		= &sysctl_sched_init_task_load_pct,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
-	},
-	{
 		.procname	= "sched_min_task_util_for_boost",
 		.data		= &sysctl_sched_min_task_util_for_boost,
 		.maxlen		= sizeof(unsigned int),
@@ -412,6 +405,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_sched_granularity_ns,
 		.extra2		= &max_sched_granularity_ns,
+	},
+	{
+		.procname	= "sched_initial_task_util",
+		.data		= &sysctl_sched_initial_task_util,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
 	},
 	{
 		.procname	= "sched_wakeup_granularity_ns",
